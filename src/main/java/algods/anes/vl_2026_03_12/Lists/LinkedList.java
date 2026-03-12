ac21586973d997df507873208c1a1cd5f107b6ff
Anes Sahbegovic <anes.sahbegovic@gmail.com>
2026-03-12 14:19:41 +0100

    d

---
+++ src/main/java/algods/anes/vl_2026_03_12/Lists/LinkedList.java
@@ -0,0 +1,52 @@
+package algods.anes.vl_2026_03_12.Lists;
+
+public class LinkedList {
+    Element anchor;
+    int size;
+
+    public LinkedList() {
+        this.anchor = new Element();
+    }
+
+    /// Hängt einen neuen Wert am Ende der Liste an.
+    public void add(int data) {
+        this.anchor.insertBefore(data);
+        this.size++;
+    }
+
+    /// Fügt ein neues Element mit dem gegebenen Wert an der gegeben Position in die
+    /// Liste ein.
+    public void insert(int pos, int data) {
+        Element current = this.anchor.next;
+
+        while (pos > 0) {
+            current = current.next;
+            pos--;
+        }
+        current.insertBefore(data);
+        this.size++;
+    }
+
+    /// Entfernt das letzte Element aus der Liste.
+    public void remove_last() {
+        this.anchor.removeBefore();
+        this.size--;
+    }
+
+    /// Entfernt das Element an der gegebene Position aus der Liste.
+    public void remove(int pos) {
+        pos++;
+        Element current = this.anchor.next;
+
+        while (pos > 0) {
+            current = current.next;
+            pos--;
+        }
+        current.removeBefore();
+        this.size--;
+    }
+
+    public int size() {
+        return this.size;
+    }
+}
---
+++ src/test/java/algods/anes/vl_2026_03_12/Lists/ElementTest.java
@@ -0,0 +1,79 @@
+package algods.anes.vl_2026_03_12.Lists;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class ElementTest {
+    @Test
+    public void testElement_new() {
+        Element anchor = new Element();
+
+        assertEquals(0, anchor.data);
+        assertEquals(anchor, anchor.next);
+        assertEquals(anchor, anchor.prev);
+    }
+
+    @Test
+    public void testElement_add_single() {
+        Element anchor = new Element();
+
+        anchor.insertBefore(42);
+
+        assertNotEquals(anchor, anchor.next);
+        assertNotEquals(anchor, anchor.prev);
+        assertEquals(0, anchor.data);
+        assertEquals(42, anchor.next.data);
+        assertEquals(anchor, anchor.next.next);
+        assertEquals(anchor, anchor.prev.prev);
+    }
+
+    @Test
+    public void testElement_add_multiple() {
+        Element anchor = new Element();
+
+        anchor.insertBefore(42);
+        anchor.insertBefore(23);
+        anchor.insertBefore(38);
+
+        assertEquals(0, anchor.data);
+        assertEquals(42, anchor.next.data);
+        assertEquals(23, anchor.next.next.data);
+        assertEquals(38, anchor.next.next.next.data);
+        assertEquals(anchor, anchor.next.next.next.next);
+
+        assertEquals(0, anchor.data);
+        assertEquals(38, anchor.prev.data);
+        assertEquals(23, anchor.prev.prev.data);
+        assertEquals(42, anchor.prev.prev.prev.data);
+        assertEquals(anchor, anchor.prev.prev.prev.prev);
+    }
+
+    @Test
+    void testElement_removeBofore() {
+        Element anchor = new Element();
+
+        anchor.insertBefore(42);
+        anchor.insertBefore(23);
+        anchor.insertBefore(38);
+
+        anchor.removeBefore();
+
+        assertEquals(0, anchor.data);
+        assertEquals(42, anchor.next.data);
+        assertEquals(23, anchor.next.next.data);
+        assertEquals(anchor, anchor.next.next.next);
+    }
+
+    @Test
+    void testElement_size() {
+        Element anchor = new Element();
+        assertEquals(0, anchor.size());
+
+        anchor.insertBefore(42);
+        anchor.insertBefore(23);
+        anchor.insertBefore(38);
+        assertEquals(3, anchor.size());
+    }
+}
