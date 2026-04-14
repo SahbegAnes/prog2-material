484c3571606a0793f78885557037b3e4f3710449
Anes Sahbegovic <anes.sahbegovic@gmail.com>
2026-04-14 09:38:25 +0200

    lets go

---
+++ src/main/java/algods/anes/vl_2026_03_24/Trees/Node.java
@@ -0,0 +1,79 @@
+package algods.anes.vl_2026_03_24.Trees;
+
+public class Node {
+    int data;
+    Node left;
+    Node right;
+
+    /// Erzeugt einen leeren Baum.
+    ///
+    /// D.h. einen Dummy ohne Kinder.
+    public Node() {
+        this.left = null;
+        this.right = null;
+    }
+
+    /// Liefert true, falls der Baum leer ist.
+    public boolean isEmpty() {
+        return this.left == null && this.right == null;
+    }
+
+    /// Fügt ein Element in den Baum ein.
+    public void add(int data) {
+        if (this.isEmpty()) {
+            this.data = data;
+            this.left = new Node();
+            this.right = new Node();
+            return;
+        }
+
+        if (data < this.data) {
+            this.left.add(data);
+        } else {
+            this.right.add(data);
+        }
+    }
+
+    /// Liefert den Knoten mit dem gegebenen `key`.
+    public Node get(int key) {
+
+        if (this.isEmpty()) {
+            return null;
+        }
+        
+        if (this.data == key){
+            return this;
+        }
+        
+        if (data < this.data) {
+            return this.left.get(key);
+        } else {
+            return this.right.get(key);
+        }
+
+    }
+
+    /// Liefert die Höhe des Baumes ab `this`.
+    public int height() {
+        if (this.isEmpty()) {
+            return 0;
+        }
+        int leftHeight = this.left.height();
+        int rightHeight = this.right.height();
+        if (leftHeight > rightHeight) {
+            return leftHeight + 1;
+        } else {
+            return rightHeight + 1;
+        }
+    }
+
+    public void printTree() {
+        if (this.isEmpty()) {
+            return;
+        }
+
+        this.left.printTree();
+        System.out.println(this.data);
+        this.right.printTree();
+    }
+}
